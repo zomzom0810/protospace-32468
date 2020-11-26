013a52e01682ea15be90e741308416df9f081ff1
zomzom0810 <74404708+zomzom0810@users.noreply.github.com>
2020-11-26 10:38:38 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
         else
             @prototype = @comment.prototype
             @comments = @prototype.comments
-            render "prototype/show"
+            render "prototypes/show"  
         end    
     end
 
